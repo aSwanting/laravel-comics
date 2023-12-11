d6d0e8ad265befa2bc6094548b30f600ed57461f
aSwanting <gab.damico@gmail.com>
2023-12-11 19:44:17 +0100

    feat: header

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,14 @@
-import './bootstrap';
-import '~resources/scss/app.scss';
-import * as bootstrap from 'bootstrap';
-import.meta.glob([
-    '../img/**'
-])
+import "./bootstrap";
+import "~resources/scss/app.scss";
+import * as bootstrap from "bootstrap";
+import.meta.glob(["../img/**"]);
+
+import "@fontsource/roboto/100.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import "@fontsource/roboto/900.css";
+import "@fontsource/roboto-condensed/300.css";
+import "@fontsource/roboto-condensed/400.css";
+import "@fontsource/roboto-condensed/700.css";
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,1 +1,49 @@
-header
+<header>
+    <div class="header-top">
+        <div class="container">
+            <ul class="top-nav">
+                <li>
+                    <a href="#">DC Power&#8480; Visa&reg;</a>
+                </li>
+                <li>
+                    <a href="#">Additional DC Sites&blacktriangledown;</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <nav class="header-nav">
+        <div class="container">
+            <div class="row align-items-center flex-nowrap">
+
+                <div class="col-auto">
+                    <a href="#">
+                        <img class="header-logo" src="{{ Vite::asset('resources/img/dc-logo.png') }}">
+                    </a>
+                </div>
+
+                <div class="col">
+                    <ul class="nav-menu">
+                        <li class="nav-item"><a href="#">Characters</a></li>
+                        <li class="nav-item"><a href="#">Comics</a></li>
+                        <li class="nav-item"><a href="#">Movies</a></li>
+                        <li class="nav-item"><a href="#">TV</a></li>
+                        <li class="nav-item"><a href="#">Games</a></li>
+                        <li class="nav-item"><a href="#">Collectibles</a></li>
+                        <li class="nav-item"><a href="#">Videos</a></li>
+                        <li class="nav-item"><a href="#">Fans</a></li>
+                        <li class="nav-item"><a href="#">News</a></li>
+                        <li class="nav-item"><a href="#">Shop <span>&blacktriangledown;</span></a></li>
+                    </ul>
+                </div>
+
+                <div class="col-auto">
+                    <div class="nav-search">
+                        <input type="text" placeholder="Search">
+                        <i class="fa-solid fa-magnifying-glass"></i>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </nav>
+</header>
